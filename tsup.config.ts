6967ef738f3a44dfd6525fb1fde9e7f7c40608df
__filename <filename@inft.kr>
2023-02-18 23:19:47 +0800

    style(eslint): added tsconfig eslint ver

---
+++ tsup.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, Options } from 'tsup';
+
+export function createTsupConfig({
+  entry = ['src/index.ts'],
+  platform = 'node',
+  format = ['esm', 'cjs'],
+  target = 'es2022',
+  skipNodeModulesBundle = true,
+  clean = true,
+  shims = true,
+  minify = false,
+  splitting = false,
+  keepNames = true,
+  dts = true,
+  sourcemap = true,
+}: Options = {}) {
+  return defineConfig({
+    entry,
+    platform,
+    format,
+    target,
+    skipNodeModulesBundle,
+    clean,
+    shims,
+    minify,
+    splitting,
+    keepNames,
+    dts,
+    sourcemap,
+  });
+}
